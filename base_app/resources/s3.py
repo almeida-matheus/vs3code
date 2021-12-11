9f4bef6e463f784d1dfd1f87984e3f9a1b6b6632
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-12-11 14:55:43 -0300

    feature: list all buckets

--- base_app/resources/s3.py
+++ base_app/resources/s3.py
@@ -1,9 +1,7 @@
 import boto3
 from botocore.exceptions import ClientError
-
 import sys
 sys.path.append("...")
-# from ...config import BUCKET_NAME, BUCKET_PATH, ACCESS_KEY, SECRET_KEY
 from config import BUCKET_NAME, ACCESS_KEY, SECRET_KEY
 
 class S3:
@@ -27,23 +25,30 @@ class S3:
     def get_bucket_name(self):
         return BUCKET_NAME
 
-    def delete_object(self,bucket_name, key):
-        ''' delete object key '''
-        try:
-            bucket = self.s3_resource.Bucket(bucket_name)
-            response = bucket.Object(key).delete()
-            return response['ResponseMetadata']['HTTPStatusCode']
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'AccessDenied':
-                return False
-        except Exception as e:
-            return False
+    def list_all_buckets(self):
+        return self.s3_resource.buckets.all()
+
+    def list_all_objects(self, bucket_name):
+        bucket = self.s3_resource.Bucket(bucket_name)
+        return bucket.objects.all()
 
     def get_object(self, bucket, key):
         ''' return object content '''
         try:
             obj =  self.s3_resource.Object(bucket, key)
             return obj.get()['Body'].read().decode('utf-8')
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'AccessDenied':
+                return False
+        except Exception as e:
+            return False
+
+    def delete_object(self,bucket_name, key):
+        ''' delete object key '''
+        try:
+            bucket = self.s3_resource.Bucket(bucket_name)
+            response = bucket.Object(key).delete()
+            return response['ResponseMetadata']['HTTPStatusCode']
         except ClientError as e:
             if e.response['Error']['Code'] == 'AccessDenied':
                 return False
@@ -75,26 +80,3 @@ class S3:
                 return False
         except Exception as e:
             return False
-
-        # self.s3_client.put_object(Body=binary_content, Bucket=BUCKET_NAME, Key='my/key/including/anotherfilename.txt')
-
-        # content="String content to write to a new S3 file"
-        # self.s3_resource.Object('my-bucket-name', 'newfile.txt').put(Body=content)
-
-        # self.s3_resource.Bucket('bucketname').upload_file('/local/file/here.txt','folder/sub/path/to/s3key')
-
-    def list_all_objects(self, bucket_name):
-        all_object_files = []
-        bucket = self.s3_resource.Bucket(bucket_name)
-        return bucket.objects.all()
-
-        # # for object_summary in bucket.objects.filter(Prefix=parent_path, Delimiter="/"): # ? first level
-        # if BUCKET_PATH:
-        #     for object_summary in bucket.objects.filter(Prefix=parent_path):
-        #         all_object_files.append(object_summary.key)
-        #         # print(object_summary.key)
-        # else:
-        #     for object_summary in bucket.objects.all():
-        #         all_object_files.append(object_summary.key)
-
-        # return all_object_files
--- base_app/view.py
+++ base_app/view.py
@@ -3,18 +3,40 @@ request, flash, redirect, url_for, session, Response
 from .resources.s3 import S3
 
 inst_s3 = S3()
-
-base_view = Blueprint("base_view", __name__, url_prefix='/s3', template_folder='templates', static_folder='static')
+base_view = Blueprint("base_view", __name__, url_prefix='/bucket', template_folder='templates', static_folder='static')
 
 @base_view.route("/", methods=["GET", "POST"])
-def index(content_object=''):
-    if session.get('content_object'): content_object=session['content_object']
-    bucket_name = inst_s3.get_bucket_name()
-    objects = inst_s3.list_all_objects(bucket_name)
-    return render_template('index.html', bucket_name=bucket_name, objects=objects, content_object=content_object)
+def index_buckets():
+    if session.get('content_object'): session.pop('content_object')
 
-@base_view.route('/delete', methods=['POST'])
-def delete():
+    if request.method == "GET":
+        buckets= inst_s3.list_all_buckets()
+        return render_template('buckets.html', buckets=buckets)
+    if request.method == "POST":
+        bucket_name = request.form['bucket_name'] # inst_s3.get_bucket_name()
+        return redirect(url_for('base_view.index_objects', bucket_name=bucket_name))
+
+@base_view.route("/<bucket_name>", methods=["GET", "POST"])
+def index_objects(bucket_name, content_object=''):
+    if session.get('content_object'): content_object=session['content_object']
+
+    objects = inst_s3.list_all_objects(bucket_name)
+    return render_template('objects.html', bucket_name=bucket_name, objects=objects, content_object=content_object)
+
+@base_view.route("/<bucket_name>/read", methods=["POST"])
+def read(bucket_name):
+    ''' get content from a file in the bucket'''
+    key = request.form['key']
+    bucket_name = inst_s3.get_bucket_name()
+    content_object = inst_s3.get_object(bucket_name, key)
+
+    flash('File read successfully')
+    session['content_object'] = content_object
+    session['key'] = key
+    return redirect(url_for('base_view.index_objects', bucket_name=bucket_name))
+
+@base_view.route('/<bucket_name>/delete', methods=['POST'])
+def delete(bucket_name):
     ''' delete an object from a file in the bucket '''
     key = request.form['key']
 
@@ -24,19 +46,7 @@ def delete():
     if (status_delete >= 200 or status_delete < 300):
         flash('File deleted successfully')
 
-    return redirect(url_for('base_view.index'))
-
-@base_view.route("/read", methods=["POST"])
-def read():
-    ''' get content from a file in the bucket'''
-    key = request.form['key']
-    bucket_name = inst_s3.get_bucket_name()
-    content_object = inst_s3.get_object(bucket_name, key)
-
-    flash('File read successfully')
-    session['content_object'] = content_object
-    session['key'] = key
-    return redirect(url_for('base_view.index'))
+    return redirect(url_for('base_view.index_objects', bucket_name=bucket_name))
 
 @base_view.route("/download", methods=["POST"])
 def download():
@@ -51,8 +61,8 @@ def download():
         headers={"Content-Disposition": "attachment;filename={}".format(key)}
     )
 
-@base_view.route("/update", methods=["POST"])
-def update(key=None):
+@base_view.route("/<bucket_name>/update", methods=["POST"])
+def update(bucket_name, key=None):
     ''' get content from a file in the html and update it in bucket file '''
     if session.get('key'): key=session['key']
     content_object = request.form['content_object']
@@ -64,4 +74,4 @@ def update(key=None):
         flash('File updated successfully')
 
     session['content_object'] = content_object
-    return redirect(url_for('base_view.index'))
+    return redirect(url_for('base_view.index_objects', bucket_name=bucket_name))
